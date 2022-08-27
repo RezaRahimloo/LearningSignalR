8da06e8b812638d0e0c5b951a4d248dde5451951
Reza Rahimloo <mr.rezarahimlou@gmail.com>
2022-08-27 14:04:07 +0430

    Added client code

--- SignalRServer/wwwroot/js/site.js
+++ SignalRServer/wwwroot/js/site.js
@@ -1,4 +1,30 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
+const connection = new signalR.HubConnectionBuilder()
+    .withUrl("/learningHub")// this is relative path we can use full path too.
+    .configureLogging(signalR.LogLevel.Information)
+    .build();
 
-// Write your JavaScript code.
+connection.on("RecieveMessage", (message) => {
+    $('#signalr-message-panel').prepend($('<div />').text(message));
+});
+
+$('#btn-broadcast').click(function (){
+    var message = $('#broadcast').val();
+    connection.invoke("BroadcastMessage", message).catch(err => console.error(err.toString()));
+});
+
+async function start() { 
+    try{
+        await connection.start();
+        console.Log('connected');
+    } catch (err){
+        console.error(err);
+        setTimeout(() => start(), 5000);
+    }
+}
+
+connection.onclose(async () => {
+    await start();
+});
+
+start();
