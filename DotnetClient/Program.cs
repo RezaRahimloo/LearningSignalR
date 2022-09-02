feedbf30d5bbd732211366458dc83c25a8673ec2
Reza Rahimloo <mr.rezarahimlou@gmail.com>
2022-09-02 23:18:47 +0430

    Added .NET client as an independant client

---
+++ DotnetClient/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+Console.WriteLine("Insert the URL of the SignalR hub");
+
+string? url = Console.ReadLine();
+
+var hubConnection = new HubConnectionBuilder()
+                        .WithUrl(url)
+                        .Build();
+
+hubConnection.On<string>("ReceiveMessage", message => Console.WriteLine($"SignalR Hub message: {message}"));
+
+try
+{
+    await hubConnection.StartAsync();
+
+    while(true)
+    {
+        string? message = string.Empty;
+
+        Console.WriteLine("Please specify action: \n0 - broadcast to all \nexit - Exit the program");
+
+        string? action = Console.ReadLine();
+
+        Console.WriteLine("Please specify the message");
+        message = Console.ReadLine();
+
+        if (action == "exit")
+        {
+            break;
+        }
+        await hubConnection.SendAsync("BroadCastMessage", message);
+    }
+}
+catch (System.Exception ex)
+{
+    Console.WriteLine(ex.Message);
+    Console.WriteLine("Press any key to exit...");
+    Console.ReadKey();
+    return;
+}
---
+++ WebSocketClient/Program.cs
@@ -0,0 +1,7 @@
+﻿using System.Net.WebSockets;
+using System.Text;
+
+Console.WriteLine("Please specify the URL of SignalR Hub with WS/WSS protocol");
+string? url = Console.ReadLine();
+
+
